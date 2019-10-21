52bc1b62d4010ac7cec38bb12d7790dab0f77c5a
sahussawud <sahussawud@hotmail.com>
2019-10-21 20:51:16 +0700

    create about us

---
+++ about/script.js
@@ -0,0 +1,37 @@
+// $(window).on('scroll', function(){
+// 	if($(window).scroll()) {
+// 		$('nav').addClass('black');
+// 		document.getElementById("logo1").scr = 'logo.png';
+// 		//$('nav').removeClass('white');
+// 	}
+// 	else {
+// 		$('nav').removeClass('black');
+// 		document.getElementById("logo1").scr = 'logo_white.png';
+// 		//$('nav').addClass('white');
+
+// 	}
+// });
+		var prevScrollpos = window.pageYOffset;
+		window.onscroll = function() {
+		var currentScrollPos = window.pageYOffset;
+		if (prevScrollpos > currentScrollPos) {
+			document.getElementById("navbar").style.top = "0";
+		} else {
+			document.getElementById("navbar").style.top = "-80px";
+		}
+		prevScrollpos = currentScrollPos;
+		}
+
+function change(element,value) {
+	element.parentElement.setAttribute('data-select',value);
+
+};
+
+
+$('choice1').on('click', function() { // when you click the div
+  $(this).removeClass('hover'); // add the class 'no-hover'
+});
+
+$('choice2').on('click', function() { // when you click the div
+  $(this).unbind("mouseenter mouseleave"); // add the class 'no-hover'
+});
