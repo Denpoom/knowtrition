dc0b18fdca161555019b07a5f27cc70ffcac8c04
kit <punyapat.kit2000@gmail.com>
2019-11-10 16:12:26 +0700

    content2 finish

--- content2/script.js
+++ content2/script.js
@@ -1,4 +1,4 @@
-$(window).on('scroll', function () {
+/*$(window).on('scroll', function () {
 	if ($(window).scrollTop()) {
 		$('nav').addClass('black');
 		$('a').addClass('white');
@@ -10,3 +10,24 @@ $(window).on('scroll', function () {
 		$('i').removeClass('white');
 	}
 });
+*/
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+	var nav = document.getElementsByTagName('NAV')[0];
+
+	var currentScrollPos = window.pageYOffset;
+	var top = window.scrollTop;
+	if (currentScrollPos === 0) {
+		nav.classList.remove('black');
+		//nav.style.background = "red";
+	}
+	else if (prevScrollpos > currentScrollPos) {
+		nav.classList.add('black');
+		nav.style.top = "0";
+	} else {
+		nav.style.top = "-90px";
+	}
+	prevScrollpos = currentScrollPos;
+}
+
