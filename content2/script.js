b966982859e5fc58f641fab700fec6ca1085067f
kit <punyapat.kit2000@gmail.com>
2019-11-10 18:14:22 +0700

    add

--- content2/script.js
+++ content2/script.js
@@ -18,13 +18,16 @@ window.onscroll = function () {
 
 	var currentScrollPos = window.pageYOffset;
 	var top = window.scrollTop;
+	var img = document.getElementById('image');
 	if (currentScrollPos === 0) {
 		nav.classList.remove('black');
+		img.src = "image/logo.png";
 		//nav.style.background = "red";
 	}
 	else if (prevScrollpos > currentScrollPos) {
 		nav.classList.add('black');
 		nav.style.top = "0";
+		img.src = "image/logo_white.png";
 	} else {
 		nav.style.top = "-90px";
 	}
