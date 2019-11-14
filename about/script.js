02c567033024f039e0f9621034f70bede1bc0b48
sahussawud <sahussawud@hotmail.com>
2019-11-14 19:13:32 +0700

    link profile

--- about/script.js
+++ about/script.js
@@ -11,30 +11,30 @@
 
 // 	}
 // });
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-	document.getElementById("navbar").style.top = "0";
-	document.getElementById("m_navbar").style.top = "0";
-	document.getElementById("nav-stripe").style.top = "10px";
-} else {
-	document.getElementById("navbar").style.top = "-80px";
-	document.getElementById("m_navbar").style.top = "-80px";
-	document.getElementById("nav-stripe").style.top = "-80px";
-}
-prevScrollpos = currentScrollPos;
-}
-function change(element,value) {
-	element.parentElement.setAttribute('data-select',value);
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+// if (prevScrollpos > currentScrollPos) {
+// 	document.getElementById("navbar").style.top = "0";
+// 	document.getElementById("m_navbar").style.top = "0";
+// 	document.getElementById("nav-stripe").style.top = "10px";
+// } else {
+// 	document.getElementById("navbar").style.top = "-80px";
+// 	document.getElementById("m_navbar").style.top = "-80px";
+// 	document.getElementById("nav-stripe").style.top = "-80px";
+// }
+// prevScrollpos = currentScrollPos;
+// }
+// function change(element,value) {
+// 	element.parentElement.setAttribute('data-select',value);
 
-};
+// };
 
 
-$('choice1').on('click', function() { // when you click the div
-  $(this).removeClass('hover'); // add the class 'no-hover'
-});
+// $('choice1').on('click', function() { // when you click the div
+//   $(this).removeClass('hover'); // add the class 'no-hover'
+// });
 
-$('choice2').on('click', function() { // when you click the div
-  $(this).unbind("mouseenter mouseleave"); // add the class 'no-hover'
-});
+// $('choice2').on('click', function() { // when you click the div
+//   $(this).unbind("mouseenter mouseleave"); // add the class 'no-hover'
+// });
