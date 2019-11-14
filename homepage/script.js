0fad73752a77464c2dd7beb9440a025ff1a431a5
sahussawud <sahussawud@hotmail.com>
2019-11-14 18:10:22 +0700

    update first page link

--- homepage/script.js
+++ homepage/script.js
@@ -2,37 +2,34 @@
 		
 		window.addEventListener("scroll", function (event) {
 			var scroll = Math.floor(this.scrollY);
-			
-			console.log("scroll mod  "+scroll%this.window.innerHeight);
-			console.log("scroll opacity  "+1/((scroll%window.innerHeight)*0.01));
+	
 
 			var change = false;
-			
-			var opa = scroll%this.window.innerHeight;
+
 			
 			
 			if(scroll < window.innerHeight){
 				this.document.querySelector('.fullscreen-bg video').style.opacity = 2/(scroll*0.01);
-				if(this.document.querySelector('source').src.slice(30) != '/asset/coverr-red-apples-1566725031062.mp4'){
+				if(this.document.querySelector('source').src != 'http://10.0.15.12/wtprojects/wtpg23/asset/coverr-red-apples-1566725031062.mp4'){
 					change = true;
 				}
-				this.document.querySelector('source').src = './asset/coverr-red-apples-1566725031062.mp4';
+				this.document.querySelector('source').src = 'http://10.0.15.12/wtprojects/wtpg23/asset/coverr-red-apples-1566725031062.mp4';
 				
 			}
 			else if(scroll < window.innerHeight*2){
 				this.document.querySelector('.fullscreen-bg video').style.opacity = 0.35+2/((scroll%window.innerHeight)*0.01);
-				if(this.document.querySelector('source').src.slice(30) != '/asset/food_foottage.mp4'){
+				if(this.document.querySelector('source').src != 'http://10.0.15.12/wtprojects/wtpg23/asset/food_foottage.mp4'){
 					change = true;
 				}
-				this.document.querySelector('source').src = './asset/food_foottage.mp4';
+				this.document.querySelector('source').src = 'http://10.0.15.12/wtprojects/wtpg23/asset/food_foottage.mp4';
 				
 				
 			}else if(scroll < window.innerHeight*3){
 				this.document.querySelector('.fullscreen-bg video').style.opacity = 0.35+2/((scroll%window.innerHeight)*0.01);
-				if(this.document.querySelector('source').src.slice(30) != '/asset/coverr-red-apples-1566725031062.mp4'){
+				if(this.document.querySelector('source').src != 'http://10.0.15.12/wtprojects/wtpg23/asset/coverr-red-apples-1566725031062.mp4'){
 					change = true;
 				}
-				this.document.querySelector('source').src = './asset/coverr-red-apples-1566725031062.mp4';
+				this.document.querySelector('source').src = 'http://10.0.15.12/wtprojects/wtpg23/asset/coverr-red-apples-1566725031062.mp4';
 				
 				
 			}else{
@@ -41,8 +38,8 @@
 			if(change){
 					
 					$(".fullscreen-bg video")[0].load();
-					
-					
+					this.console.log("load");
+						
 				
 			}
 			
@@ -112,7 +109,6 @@ if(!document.getElementById('id1')) {
 	document.querySelector('title').innerText = document.querySelector('.boxtopic').innerText+ ' - Knowtrition เว็บไซต์เพื่อเรียนรู้เกี่ยวกับวัตถุดิบ';
 }
 
-function test(){
-	alert("Test");
-}
+
+
 
